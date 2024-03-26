b4284e954719586fc6d1331900445f8dc9fa0ede
Dan Burzo <dan@danburzo.ro>
2024-03-26 18:02:30 +0200

    Fix test runner

--- index.js
+++ index.js
@@ -12,6 +12,69 @@ import remarkGfm from 'remark-gfm';
 import { SKIP, visit } from 'unist-util-visit';
 import { fromHtml } from 'hast-util-from-html';
 import { toMdast } from 'hast-util-to-mdast';
+/*
+	Remark plugin to fix trailing whitespace in inline nodes.
+	See: https://github.com/orgs/syntax-tree/discussions/60
+*/
+function remarkTrailingWhitespace() {
+	return function (tree, file) {
+		visit(tree, ['strong', 'emphasis'], (node, index, parent) => {
+			// Remove empty nodes
+			if (!node.children.length) {
+				parent.children.splice(index, 1);
+				return SKIP;
+			}
+
+			const first = node.children[0];
+
+			// Remove image nodes from within emphasis nodes
+			if (first.type === 'image') {
+				parent.children.splice(index, 1, first);
+				return SKIP;
+			}
+
+			// Remove trailing whitespace at the beginning
+			if (first.type === 'text') {
+				const start_match = first.value.match(/^(\s+)(.*)/);
+				if (start_match) {
+					const [_, start_ws, start_text] = start_match;
+					parent.children.splice(index, 0, {
+						type: 'text',
+						value: start_ws
+					});
+					if (start_text) {
+						// has non-whitespace content
+						first.value = start_text;
+					} else {
+						// all-whitespace content
+						node.children.shift();
+					}
+					// Re-visit this node
+					return [SKIP, index - 1];
+				}
+			}
+
+			const last = node.children[node.children.length - 1];
+			if (last.type === 'text') {
+				const end_match = last.value.match(/(.*?)(\s+)$/);
+				if (end_match) {
+					const [_, end_text, end_ws] = end_match;
+					parent.children.splice(index + 1, 0, {
+						type: 'text',
+						value: end_ws
+					});
+					if (end_text) {
+						last.value = end_text;
+					} else {
+						node.children.pop();
+					}
+				}
+			}
+
+			return SKIP;
+		});
+	};
+}
 
 export async function slurp(stream) {
 	let arr = [],
@@ -40,11 +103,16 @@ function remarkParseHtml() {
 }
 
 export function getHtmlToMdProcessor(opts = {}) {
-	return unified()
-		.use(rehypeParse)
-		.use(rehypeRemark)
-		.use(remarkGfm)
-		.use(remarkStringify, opts);
+	return (
+		unified()
+			.use(rehypeParse)
+			.use(rehypeRemark)
+			.use(remarkGfm)
+			// Not enabled yet, as it doesn’t treat some corner cases
+			// See: https://github.com/danburzo/trimd/issues/5
+			// .use(remarkTrailingWhitespace)
+			.use(remarkStringify, opts)
+	);
 }
 
 export function getMdToMdProcessor(opts = {}) {
--- test/markdown.test.js
+++ test/markdown.test.js
@@ -3,7 +3,24 @@ import assert from 'node:assert';
 import { getHtmlToMdProcessor } from '../index.js';
 
 test('trailing whitespace', async t => {
-	const input =
-		'<span style="font-weight: 400;">Select the </span><strong>Project Window </strong><span style="font-weight: 400;">in your Dashboard.</span>';
-	assert.equal(await getHtmlToMdProcessor().process(input).then(String), '');
+	const testcases = [
+		{
+			input: '<span style="font-weight: 400;">Select the </span><strong>Project Window </strong><span style="font-weight: 400;">in your Dashboard.</span>',
+			expected: 'Select the **Project Window** in your Dashboard.\n'
+		},
+		{
+			input: '<em>italic<strong> bold</strong></em>',
+			expected: ''
+		}
+	];
+	await Promise.all(
+		testcases.map(async testcase => {
+			assert.equal(
+				await getHtmlToMdProcessor()
+					.process(testcase.input)
+					.then(String),
+				testcase.expected
+			);
+		})
+	);
 });
